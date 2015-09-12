9006e36b3369e020ef1278af881f63f2e142f50d
Dag <dagmawi@vt.edu>
2015-09-12 14:57:34 -0400

    Initial Commit

---
+++ lifeboard2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lifeboard2
+//
+//  Created by Dag Yeshiwas on 9/12/15.
+//  Copyright (c) 2015 Dag Yeshiwas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
